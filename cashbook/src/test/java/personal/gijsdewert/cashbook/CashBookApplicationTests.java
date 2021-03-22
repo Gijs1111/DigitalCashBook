eae22eb4fc453eb3df923db7aa7f5127e07073c0
Gijs de Wert <363905@student.fontys.nl>
2021-03-22 16:04:57 +0100

    Initialization of the Spring Boot project
    Created a new project

---
+++ cashbook/src/test/java/personal/gijsdewert/cashbook/CashBookApplicationTests.java
@@ -0,0 +1,13 @@
+package personal.gijsdewert.cashbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
